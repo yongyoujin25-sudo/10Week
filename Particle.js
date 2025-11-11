c742c5d2a706042279400c9102f1c5132663a6dd
yongyoujin25-sudo <yongyoujin25@gmail.com>
2025-11-11 11:41:28 +0900

    파티클 클래스 만들기

--- Particle.js
+++ Particle.js
@@ -1,32 +1,35 @@
 // A simple Particle class
-let Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.lifespan = 255;
-};
 
-Particle.prototype.run = function() {
+class Particle {
+    constructor(position) {
+        this.acceleration = createVector(0, 0.05);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
+}
+
+run(){
   this.update();
   this.display();
-};
+}
 
-// Method to update position
-Particle.prototype.update = function(){
+update(){
   this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
   this.lifespan -= 2;
-};
 
-// Method to display
-Particle.prototype.display = function() {
+}
+
+display(){
   stroke(200, this.lifespan);
   strokeWeight(2);
   fill(127, this.lifespan);
   ellipse(this.position.x, this.position.y, 12, 12);
-};
 
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
+}
+
+IsDead(){
   return this.lifespan < 0;
-};
+}
+
+}
