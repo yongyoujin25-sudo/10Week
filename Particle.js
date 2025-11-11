ba9a6b3b2d499c2658e3b6c1ccb6fde887c9452c
yongyoujin25-sudo <yongyoujin25@gmail.com>
2025-11-11 11:26:30 +0900

    파티클시스템 클래스 분리하기

--- Particle.js
+++ Particle.js
@@ -30,22 +30,3 @@ Particle.prototype.display = function() {
 Particle.prototype.isDead = function(){
   return this.lifespan < 0;
 };
-
-let ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
-    let p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
-    }
-  }
-};
