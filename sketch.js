d356699b1f013625875409311d7fdd89134c2c1a
yongyoujin25-sudo <yongyoujin25@gmail.com>
2025-11-11 11:15:17 +0900

    p5js 구조 만들기

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let system;
+
+function setup() {
+  createCanvas(720, 400);
+  system = new ParticleSystem(createVector(width / 2, 50));
+}
+
+function draw() {
+  background(51);
+  system.addParticle();
+  system.run();
+}
