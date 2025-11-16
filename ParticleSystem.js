61cb8121ea0bbdbbce6cc732d65c6687bc1f6803
yongyoujin25-sudo <yongyoujin25@gmail.com>
2025-11-17 04:44:51 +0900

    위치 변경, 마우스 벡터 추가

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -8,6 +8,10 @@ addParticle() {
     this.particles.push(new Particle(this.origin));
 }
 
+setPosition(position) {
+  this.origin = position.copy();
+}
+
 run() {
 for (let i = this.particles.length-1; i >= 0; i--) {
     let p = this.particles[i];
--- sketch.js
+++ sketch.js
@@ -2,12 +2,14 @@ let system;
 
 function setup() {
   createCanvas(720, 400);
-  
   system = new ParticleSystem(createVector(width / 2, 50));
 }
 
 function draw() {
   background(51);
+
+  system.setPosition(createVector(mouseX,mouseY));
+
   system.addParticle();
   system.run();
 }
